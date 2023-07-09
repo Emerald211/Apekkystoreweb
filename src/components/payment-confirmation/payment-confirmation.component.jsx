7804c981998c26bc38ec35cfa9db293e810cbf77
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2023-07-09 13:33:45 +0100

    fix

--- src/components/payment-confirmation/payment-confirmation.component.jsx
+++ src/components/payment-confirmation/payment-confirmation.component.jsx
@@ -52,106 +52,107 @@ const PaymentConfirmation = () => {
     return () => unsubscribe();
   }, [storedOrder]);
 
-  const sendEmail = async () => {
-    const completedOrder = {
-      id: storedOrder.id,
-      email: storedOrder.email,
-      name: storedOrder.name,
-      items: storedOrder.items,
-      amount: storedOrder.amount,
-      status: "COMPLETED",
-      time: storedOrder.time,
-      payerid: storedOrder.payerid,
-      address: storedOrder.address,
-      country: storedOrder.country,
-      deliverytime: storedOrder.deliverytime,
-    };
-    console.log(completedOrder);
-  
-    const products = completedOrder.items;
-  
-    const productDetails = products.map((product) => {
-      return `
-        Product: ${product.name}
-        Image URL: ${product.imageUrl}
-        Price: ${product.price} Euros
-        Quantity: ${product.quantity}
-      `;
-    });
-  
-    const formattedProductDetails = productDetails.join("\n");
-  
-    const sendPaymentConfirmationEmail = async () => {
-      const templateParams = {
-        name: completedOrder.name,
-        order_id: completedOrder.id,
-        amount: completedOrder.amount,
-        paymentMethod: "PAYPAL",
-        items: formattedProductDetails,
+  useEffect(() => {
+    const sendEmail = async () => {
+      const completedOrder = {
+        id: storedOrder.id,
+        email: storedOrder.email,
+        name: storedOrder.name,
+        items: storedOrder.items,
+        amount: storedOrder.amount,
+        status: "COMPLETED",
+        time: storedOrder.time,
+        payerid: storedOrder.payerid,
+        address: storedOrder.address,
+        country: storedOrder.country,
+        deliverytime: storedOrder.deliverytime,
       };
-  
+      console.log(completedOrder);
+
+      const products = completedOrder.items;
+
+      const productDetails = products.map((product) => {
+        return `
+          Product: ${product.name}
+          Image URL: ${product.imageUrl}
+          Price: ${product.price} Euros
+          Quantity: ${product.quantity}
+        `;
+      });
+
+      const formattedProductDetails = productDetails.join("\n");
+
+      const sendPaymentConfirmationEmail = async () => {
+        const templateParams = {
+          name: completedOrder.name,
+          order_id: completedOrder.id,
+          amount: completedOrder.amount,
+          paymentMethod: "PAYPAL",
+          items: formattedProductDetails,
+        };
+
+        try {
+          const response = await emailjs.send(
+            "service_x1xb88n",
+            "template_vswwvhp",
+            templateParams
+          );
+          console.log(
+            "Payment confirmation email sent to the customer:",
+            response.status,
+            response.text
+          );
+        } catch (error) {
+          console.error(
+            "Error sending payment confirmation email to the customer:",
+            error
+          );
+        }
+      };
+
+      const sendPaymentNotificationToSeller = async () => {
+        const templateParams = {
+          order_id: completedOrder.id,
+          amount: completedOrder.amount,
+          customerName: completedOrder.name,
+          customerEmail: completedOrder.email,
+          paymentMethod: "PAYPAL",
+          items: formattedProductDetails,
+          address: completedOrder.address,
+          country: completedOrder.country,
+          deliverytime: completedOrder.deliverytime,
+        };
+
+        try {
+          const response = await emailjs.send(
+            "service_x1xb88n",
+            "template_csfo85y",
+            templateParams
+          );
+          console.log(
+            "Payment notification email sent to the seller:",
+            response.status,
+            response.text
+          );
+        } catch (error) {
+          console.error(
+            "Error sending payment notification email to the seller:",
+            error
+          );
+        }
+      };
+
       try {
-        const response = await emailjs.send(
-          "service_x1xb88n",
-          "template_vswwvhp",
-          templateParams
-        );
-        console.log(
-          "Payment confirmation email sent to the customer:",
-          response.status,
-          response.text
-        );
+        await sendPaymentConfirmationEmail();
+        await sendPaymentNotificationToSeller();
+        alert("Order completed");
       } catch (error) {
-        console.error(
-          "Error sending payment confirmation email to the customer:",
-          error
-        );
+        console.error("Error during email sending:", error);
       }
     };
-  
-    const sendPaymentNotificationToSeller = async () => {
-      const templateParams = {
-        order_id: completedOrder.id,
-        amount: completedOrder.amount,
-        customerName: completedOrder.name,
-        customerEmail: completedOrder.email,
-        paymentMethod: "PAYPAL",
-        items: formattedProductDetails,
-        address: completedOrder.address,
-        country: completedOrder.country,
-        deliverytime: completedOrder.deliverytime,
-      };
-  
-      try {
-        const response = await emailjs.send(
-          "service_x1xb88n",
-          "template_csfo85y",
-          templateParams
-        );
-        console.log(
-          "Payment notification email sent to the seller:",
-          response.status,
-          response.text
-        );
-      } catch (error) {
-        console.error(
-          "Error sending payment notification email to the seller:",
-          error
-        );
-      }
-    };
-  
-    try {
-      await sendPaymentConfirmationEmail();
-      await sendPaymentNotificationToSeller();
-      alert("Order completed");
-    } catch (error) {
-      console.error("Error during email sending:", error);
-    }
-  };
-  
-  sendEmail();
-  
+
+    sendEmail();
+  }, []); // Empty dependency array to run once when the component mounts
 
   const navigate = useNavigate();
   return (
